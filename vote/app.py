f7c115248cb5a0ca23546de764af4801d6555b41
nileshpatil86 <nileshpatil41@yahoo.com>
2026-09-05 17:18:32 +0530

    Wrong code inserted for testing

--- vote/app.py
+++ vote/app.py
@@ -1,13 +1,66 @@
+# from flask import Flask, render_template, request, make_response, g
+# from redis import Redis
+# import os
+# import socket
+# import random
+# import json
+# import logging
+
+# option_a = os.getenv('OPTION_A', "Cats")
+# option_b = os.getenv('OPTION_B', "Dogs")
+# hostname = socket.gethostname()
+
+# app = Flask(__name__)
+
+# gunicorn_error_logger = logging.getLogger('gunicorn.error')
+# app.logger.handlers.extend(gunicorn_error_logger.handlers)
+# app.logger.setLevel(logging.INFO)
+
+# def get_redis():
+#     if not hasattr(g, 'redis'):
+#         g.redis = Redis(host=os.getenv('REDIS_HOST', "redis"), db=0, socket_timeout=5)
+#     return g.redis
+
+# @app.route("/", methods=['POST','GET'])
+# def hello():
+#     voter_id = request.cookies.get('voter_id')
+#     if not voter_id:
+#         voter_id = hex(random.getrandbits(64))[2:-1]
+
+#     vote = None
+
+#     if request.method == 'POST':
+#         redis = get_redis()
+#         vote = request.form['vote']
+#         app.logger.info('Received vote for %s', vote)
+#         data = json.dumps({'voter_id': voter_id, 'vote': vote})
+#         redis.rpush('votes', data)
+
+#     resp = make_response(render_template(
+#         'index.html',
+#         option_a=option_a,
+#         option_b=option_b,
+#         hostname=hostname,
+#         vote=vote,
+#     ))
+#     resp.set_cookie('voter_id', voter_id)
+#     return resp
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+
+
 from flask import Flask, render_template, request, make_response, g
 from redis import Redis
 import os
 import socket
 import random
-import json
+import jsonn  # BUG 1: typo in module name -> ModuleNotFoundError on startup
 import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_b = os.getenv('OPTION_B' "Dogs")  # BUG 2: missing comma -> SyntaxError (adjacent string literals silently concat, but this changes behavior/args)
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -25,15 +78,15 @@ def get_redis():
 def hello():
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
+    voter_id = hex(random.getrandbits(64))[2:-1]  # BUG 3: bad indentation -> IndentationError
 
     vote = None
 
     if request.method == 'POST':
         redis = get_redis()
-        vote = request.form['vote']
+        vote = request.form['votes']  # BUG 4: wrong form key -> KeyError at runtime (form field is actually 'vote')
         app.logger.info('Received vote for %s', vote)
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        data = jsonn.dumps({'voter_id': voter_id, 'vote': vote})
         redis.rpush('votes', data)
 
     resp = make_response(render_template(
